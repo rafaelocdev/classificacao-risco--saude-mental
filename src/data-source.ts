82f0f0f2480d4b6835be06e0919be303b21bc91c
Rafael Carvalho <65735479+rafaelocdev@users.noreply.github.com>
2022-06-07 09:34:01 -0300

    feat: created .env.example and update data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,10 @@ config();
 
 const AppDataSourceDev = new DataSource({
   type: "postgres",
-  url: process.env.DATABASE_URL,
+  host: "postgresDB",
+  database: process.env.POSTGRES_DB,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
   logging: true,
   entities: [path.join(__dirname, "/entities/**/*.{ts,js}")],
   migrations: [path.join(__dirname, "/migrations/**/*.{ts,js}")],
@@ -15,7 +18,10 @@ const AppDataSourceDev = new DataSource({
 
 const AppDataSourceProd = new DataSource({
   type: "postgres",
-  url: process.env.DATABASE_URL,
+  host: "postgresDB",
+  database: process.env.POSTGRES_DB,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
   ssl: { rejectUnauthorized: false },
   logging: true,
   entities: [path.join(__dirname, "/entities/**/*.{ts,js}")],
