dfbd6bd3b5dcb98490222c1d99d89db30f39655b
Victor Nunes <victornunes@pop-os.localdomain>
2022-06-09 09:49:44 -0300

    feat: added errorHandler in app.use

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { errorHandler } from "./errors/errors";
 
 const app = express();
 app.use(express.json());
+
+app.use((err: Error, _: Request, res: Response, __: NextFunction) => {
+  return errorHandler(err, res);
+});
 
 export default app;
