7ab8f598421da1ac42805f84b98ac4711fd30d33
Rafael Carvalho <65735479+rafaelocdev@users.noreply.github.com>
2022-06-11 12:18:38 -0300

    fix: resolved importation problem on index from repositories folder

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -2,7 +2,7 @@ import resultMhRiskRepo from "./resultMhRisk.repository";
 import queryMhRiskRepo from "./queryMhRisk.repository";
 import dataRepo from "./data.repository";
 import clientRepo from "./client.repository";
-import employeeRepo from "./resultMhRisk.repository";
+import employeeRepo from "./employee.repository";
 import onDutyRepo from "./onDuty.repository";
 import appointmentRepo from "./appointment.repository";
 
