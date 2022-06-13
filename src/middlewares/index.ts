bff741d8f66a04baf033921ea7017a4e95641ad6
andresmartins910 <andresmartins910@gmail.com>
2022-06-13 15:33:18 -0300

    feat: validateIsAdmin middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,4 +1,5 @@
 import validateTokenMiddleware from "./validateToken.middleware";
 import validateSchema from "./validateSchema.middleware";
+import validateIsAdmin from "./validateIsAdmin.middleware";
 
-export { validateTokenMiddleware, validateSchema };
+export { validateTokenMiddleware, validateSchema, validateIsAdmin };
