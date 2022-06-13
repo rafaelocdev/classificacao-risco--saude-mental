0247890ffc82040cdb8cb6c37766d1d066aafd59
Brunobgr08 <brunobgr0810@gmail.com>
2022-06-13 15:13:42 -0300

    feat: starting patch client service

--- src/services/admin.services.ts
+++ src/services/admin.services.ts
@@ -1,1 +1,11 @@
-export default class AdminService {}
+import { Request } from "express";
+import clientRepository from "../repositories/client.repository";
+import dataRepository from "../repositories/data.repository";
+
+class AdminService {
+  updateClient = async (id: string, { validated, decoded }: Request) => {
+    const foundUser = {};
+  };
+}
+
+export default new AdminService();
