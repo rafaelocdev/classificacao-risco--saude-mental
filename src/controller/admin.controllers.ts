427f918a917572d74939a7a408552ecc85c61f1b
andresmartins910 <andresmartins910@gmail.com>
2022-06-16 11:49:50 -0300

    feat: register client services and minor fixes

--- src/controller/admin.controllers.ts
+++ src/controller/admin.controllers.ts
@@ -1,38 +1,37 @@
 import { Request, Response } from "express";
 import { adminService } from "../services";
 
-export class AdminController {  
-  
+export class AdminController {
   registerClient = async (req: Request, res: Response) => {
     const newClient = await adminService.registerClient(req);
 
     return res.status(201).json(newClient);
-  }; 
-  
+  };
 
   getClients = async (req: Request, res: Response) => {
     const clients = await adminService.getClients();
 
     return res.status(200).json({ clients });
   };
-    
-  
+
   deleteClient = async (req: Request, res: Response) => {
     const { clientId } = req.params;
     const deletedClient = await adminService.deleteClient(clientId);
 
     return res.status(200).json(deletedClient);
   };
-    
-    
+
+  registerEmployee = async (req: Request, res: Response) => {
+    const newEmployee = await adminService.registerEmployee(req);
+
+    return res.status(201).json(newEmployee);
+  };
 
   getAllEmployees = async (_: Request, res: Response) => {
     const employees = await adminService.getAllEmployees();
 
     return res.status(200).json({ employees });
   };
-  
-  
 }
 
 export default new AdminController();
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,12 +2,17 @@ import {
   registerClientSchema,
   serializedData,
 } from "./admin/registerClient.schema";
+
+import { registerEmployeeSchema, serializeEmployeeData } from "./admin";
+
 import loginSchema from "./login/login.schema";
 import { serializedClient, serializedClientWithAppointments } from "./doctor";
 
 export {
   registerClientSchema,
   serializedData,
+  registerEmployeeSchema,
+  serializeEmployeeData,
   loginSchema,
   serializedClient,
   serializedClientWithAppointments,
