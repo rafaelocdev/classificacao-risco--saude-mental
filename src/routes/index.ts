4a8f2f1889c6d013234bfca2d40cd5cf3c2e75e4
andresmartins910 <andresmartins910@gmail.com>
2022-06-21 15:40:14 -0300

    feat: 404 [not found] route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import adminRouter from "./admin.routes";
 import doctorRouter from "./doctor.routes";
 import nurseRouter from "./nurse.routes";
-import { Express } from "express";
+import { Express, Request, Response } from "express";
 import loginRouter from "./login.routes";
 
 const registerRoutes = (app: Express) => {
@@ -9,6 +9,13 @@ const registerRoutes = (app: Express) => {
   app.use("", doctorRouter);
   app.use("", nurseRouter);
   app.use(loginRouter);
+
+  // all invalid routes fall into this
+  app.all("*", (_: Request, res: Response) => {
+    return res.status(404).json({
+      message: "Not found",
+    });
+  });
 };
 
 export default registerRoutes;
