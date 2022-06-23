381b8659e583b605039960bf331a2508d1210279
Nicky <nicollyporfirio12@gmail.com>
2022-06-23 09:54:34 -0300

    update: updated schema

--- src/schemas/admin/getAllEmployees.schema.ts
+++ src/schemas/admin/getAllEmployees.schema.ts
@@ -1,16 +1,28 @@
 import * as yup from "yup";
 
 const getAllEmployeesSchema = yup.array().of(
-    yup.object().shape({
-        id: yup.string(),
-        name: yup.string(),
-        password: yup.string(),
-        job: yup.string(),
-        speciality: yup.string(),
-        register: yup.string(),
-        active: yup.bool(),
-        data_id: yup.string()
-    })
-).required();
+  yup.object().shape({
+    id: yup.string().required(),
+    name: yup.string().required(),
+    register: yup.string().required(),
+    job: yup.string().required(),
+    specialty: yup.string().required(),
+    isActive: yup.bool().optional(),
+    data: yup.object().shape({
+      id: yup.string().required(),
+      cpf: yup.string().required(),
+      birthday: yup.string().required(),
+      gender: yup.string().required(),
+      email: yup.string().email().required(),
+      mobile: yup.string().required(),
+      street: yup.string().required(),
+      number: yup.string().required(),
+      complement: yup.string().optional(),
+      zip: yup.string().required(),
+      city: yup.string().required(),
+      state: yup.string().required(),
+    }),
+  })
+);
 
 export default getAllEmployeesSchema;
