871f06bda6d2de851a3242fff7da0c1ae7d9d522
Rafael Carvalho <65735479+rafaelocdev@users.noreply.github.com>
2022-06-23 03:07:42 -0300

    chore: setting cors

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import "express-async-errors";
 import express, { NextFunction, Request, Response } from "express";
 import { errorHandler } from "./errors/errors";
 import registerRoutes from "./routes";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 registerRoutes(app);
 
 app.use((err: Error, _: Request, res: Response, __: NextFunction) => {
